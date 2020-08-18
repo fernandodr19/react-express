1589bb5c9498a5e4b04687f504e32d7469061178
Fernando Da Rós <fernando.ros@stone.com.br>
2020-08-17 23:17:49 -0300

    Edit proxy url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
+app.get('https://fdr-react-express.herokuapp.com/api/customers', (req, res) => {
     // Load from DB here
     const customers = [
         {id: 1, firstName: 'John', lastName: 'Doe1'},
