1dd4a2501ea9807d3d3d77e565fe72576ae6ff00
Fernando Da Rós <fernando.ros@stone.com.br>
2020-08-17 22:12:12 -0300

    Fetch data from backend

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+    constructor() {
+        super()
+        this.state = {
+            customers: []
+        }
+    }
+
+    // Following code runs automatically once componet is mounted
+    componentDidMount() {
+        // We do not have to type full path because we already defined the proxy on client package.json
+        fetch('/api/customers')
+            .then(res => res.json())
+            .then(customers => this.setState({customers: customers}, () => console.log('Customers fetched...', customers)))
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Customers</h2>
+                <ul>
+                    {this.state.customers.map(customer =>
+                        <li key={customer.id}>{customer.firstName} {customer.lastName}</li>)}
+                </ul>
+            </div>
+          );
+    }
+}
+
+export default Customers;
